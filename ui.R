988783b60951572d7363082c5e72c01dd44b3065
Ryan Kinzer <ryank@nezperce.org>
2019-05-28 21:03:39 -0600

    testing realign of local with remote

--- ui.R
+++ ui.R
@@ -218,15 +218,15 @@ shinyUI(
                         tabPanel(tags$a("Hydro-system Operations", href = "https://nptfisheries.shinyapps.io/pitph2/", target = '_blank')),
                         tabPanel(tags$a("PITtrackR", href = "https://nptfisheries.shinyapps.io/PITtrackR/", target = '_blank'))
                         ),
-             # navbarMenu("Reporting",
-             #            tabPanel("Juvenile Report", id = 'juv_report', value = 'tab_juvreport',
-             #                                tags$iframe(style="height:900px; width:100%; scrolling=yes", 
-             #                                            src="juv_draft1.pdf")#, #in www folder
-             #                      ),
-             #            tabPanel("Adult Report", id = 'adult_report', value = 'tab_adultreport',
-             #                     tags$iframe(style="height:900px; width:100%; scrolling=yes", 
-             #                                             src="juv_draft1.pdf")) #in www folder)
-             #           ),
+             navbarMenu("Reporting",
+                        tabPanel("Juvenile Report", id = 'juv_report', value = 'tab_juvreport',
+                                            tags$iframe(style="height:900px; width:100%; scrolling=yes",
+                                                        src="juv_draft1.pdf")#, #in www folder
+                                  ),
+                        tabPanel("Adult Report", id = 'adult_report', value = 'tab_adultreport',
+                                 tags$iframe(style="height:900px; width:100%; scrolling=yes",
+                                                         src="juv_draft1.pdf")) #in www folder)
+                       ),
              tabPanel("Raw Data", id = 'raw_data', value = 'tab_rawdata',
                       fluidPage(
                           fluidRow(
