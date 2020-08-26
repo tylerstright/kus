7d6e21d5442921eac1564d1f5f27e1fd16e2d6fe
Tyler Stright <tylers@nezpercetribe.org>
2020-08-26 09:07:52 -0600

    improved contact info module to include extension if necessary. Users table now has extension as well.

--- R/contactInfoServer.R
+++ R/contactInfoServer.R
@@ -3,15 +3,13 @@ contactInfoServer <- function(id, employee_, position_=NULL) { # or should it be
     id,
     
     function(input, output, session) {
-      
+      # position will auto fill from users table unless specified in argument
       output$position <- renderUI({
         if(is.null(position_)) {
           h4(paste0(users[match(employee_, users$Name), "Position"], ':'), style = 'color:white')
         } else {
           h4(paste0(position_, ':'), style = 'color:white')
         }
-        
-        # h4(paste0(gsub('[:digit:]', '', id), ':'), style = 'color:white')
       })
       
       output$name <- renderUI({
@@ -19,7 +17,13 @@ contactInfoServer <- function(id, employee_, position_=NULL) { # or should it be
       })
       
       output$phone <- renderUI({
-        h4(users[match(employee_, users$Name), "WorkPhone"], style = 'color:white') 
+        if(is.na(users[match(employee_, users$Name), "Extension"]) |
+           str_extract(users[match(employee_, users$Name), "WorkPhone"], '[:digit:]{4}$') == users[match(employee_, users$Name), "Extension"]) {
+          h4(users[match(employee_, users$Name), "WorkPhone"], style = 'color:white') 
+        } else {
+          h4(paste(users[match(employee_, users$Name), "WorkPhone"], 'Ext:',
+                   users[match(employee_, users$Name), "Extension"]), style = 'color:white') 
+        }
       })
       
       output$email <- renderUI({
